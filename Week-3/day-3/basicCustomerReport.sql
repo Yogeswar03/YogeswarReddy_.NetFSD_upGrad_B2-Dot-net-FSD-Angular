0876998c9bbf18ee788d37e104599ffb68b101fd
Yogeswar03 <yogi092503@gmail.com>
2026-03-06 13:32:44 +0530

    week-3 day-3

---
+++ Week-3/day-3/basicCustomerReport.sql
@@ -0,0 +1,13 @@
+-- Retrieve customer orders with Pending or Completed status
+
+SELECT 
+    c.first_name,
+    c.last_name,
+    o.order_id,
+    o.order_date,
+    o.order_status
+FROM customers c
+INNER JOIN orders o
+ON c.customer_id = o.customer_id
+WHERE o.order_status = 1 OR o.order_status = 4
+ORDER BY o.order_date DESC;
