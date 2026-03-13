e4c1a878e6a151ce741a26f66900fe39dc11add7
Yogeswar03 <yogi092503@gmail.com>
2026-03-13 23:40:47 +0530

    Added Week 4 Day 3 4 5 projects

---
+++ Week-4/day-3/EmployeesBonusCalculator/EmployeesBonusCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class EmployeeBonusCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Salary: ");
+        double salary = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Experience (years): ");
+        int experience = Convert.ToInt32(Console.ReadLine());
+
+        double bonusRate;
+
+        // Using if-else for bonus rule
+        if (experience < 2)
+        {
+            bonusRate = 0.05;
+        }
+        else if (experience <= 5)
+        {
+            bonusRate = 0.10;
+        }
+        else
+        {
+            bonusRate = 0.15;
+        }
+
+        double bonus = salary * bonusRate;
+
+        // Using ternary operator for final salary calculation
+        double finalSalary = bonus > 0 ? salary + bonus : salary;
+
+        Console.WriteLine("\nEmployee: " + name);
+        Console.WriteLine("Bonus: " + bonus.ToString("F2"));
+        Console.WriteLine("Final Salary: " + finalSalary.ToString("F2"));
+    }
+}
---
+++ Week-4/day-3/SimpleCalculatorUsingSwitch/SimpleCalculatorUsingSwitch/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        double num1, num2;
+        char op;
+
+        Console.Write("Enter First Number: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Second Number: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Operator (+, -, *, /): ");
+        op = Convert.ToChar(Console.ReadLine());
+
+        double result;
+
+        switch (op)
+        {
+            case '+':
+                result = num1 + num2;
+                Console.WriteLine("Result: " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                Console.WriteLine("Result: " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                Console.WriteLine("Result: " + result);
+                break;
+
+            case '/':
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Error: Division by zero is not allowed.");
+                }
+                else
+                {
+                    result = num1 / num2;
+                    Console.WriteLine("Result: " + result);
+                }
+                break;
+
+            default:
+                Console.WriteLine("Invalid Operator.");
+                break;
+        }
+    }
+}
